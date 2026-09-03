23e6b7061ea8f613f197d4c600c3c91756be1d73
KirtanKarwa <kirtankarwa2y@gmail.com>
2026-09-04 00:43:42 +0530

    Implement 2-stage cinematic over-the-head camera arc transition to computer desk

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -125,42 +125,99 @@ const Scene = () => {
           y: camera.position.y,
           z: camera.position.z,
         };
-        gsap.to(camera.position, {
-          x: -1.8,
-          y: 12.2,
-          z: 19.5,
-          duration: 1.8,
-          ease: "power3.inOut",
+
+        const openTl = gsap.timeline({
+          onComplete: () => setIsResumeDeskOpen(true),
         });
-        gsap.to(camera.rotation, {
-          x: -0.04,
-          y: 0.12,
-          z: 0,
-          duration: 1.8,
-          ease: "power3.inOut",
-        });
-        setIsResumeDeskOpen(true);
+
+        // Stage 1: Arc up over top of character while facing character
+        openTl
+          .to(camera.position, {
+            x: 0,
+            y: 19.5,
+            z: 16.0,
+            duration: 0.9,
+            ease: "power2.in",
+          })
+          .to(
+            camera.rotation,
+            {
+              x: -0.38,
+              y: 0,
+              z: 0,
+              duration: 0.9,
+              ease: "power2.in",
+            },
+            0
+          )
+          // Stage 2: Glide down to the desk surface beside the monitor
+          .to(camera.position, {
+            x: -1.5,
+            y: 11.8,
+            z: 18.0,
+            duration: 1.1,
+            ease: "power2.out",
+          })
+          .to(
+            camera.rotation,
+            {
+              x: -0.05,
+              y: 0.1,
+              z: 0,
+              duration: 1.1,
+              ease: "power2.out",
+            },
+            "<"
+          );
       };
 
       const handleCloseResumeDesk = () => {
+        setIsResumeDeskOpen(false);
         const targetX = initialCamPos.current?.x || 0;
         const targetY = initialCamPos.current?.y || 13.1;
         const targetZ = initialCamPos.current?.z || 24.7;
-        gsap.to(camera.position, {
-          x: targetX,
-          y: targetY,
-          z: targetZ,
-          duration: 1.8,
-          ease: "power3.inOut",
-        });
-        gsap.to(camera.rotation, {
-          x: 0,
-          y: 0,
-          z: 0,
-          duration: 1.8,
-          ease: "power3.inOut",
-        });
-        setIsResumeDeskOpen(false);
+
+        const closeTl = gsap.timeline();
+
+        // Stage 1: Arc back up over top of character
+        closeTl
+          .to(camera.position, {
+            x: 0,
+            y: 19.5,
+            z: 16.0,
+            duration: 0.9,
+            ease: "power2.in",
+          })
+          .to(
+            camera.rotation,
+            {
+              x: -0.38,
+              y: 0,
+              z: 0,
+              duration: 0.9,
+              ease: "power2.in",
+            },
+            0
+          )
+          // Stage 2: Return to initial front view
+          .to(camera.position, {
+            x: targetX,
+            y: targetY,
+            z: targetZ,
+            duration: 1.1,
+            ease: "power2.out",
+          })
+          .to(
+            camera.rotation,
+            {
+              x: 0,
+              y: 0,
+              z: 0,
+              duration: 1.1,
+              ease: "power2.out",
+            },
+            "<"
+          );
       };
 
       window.addEventListener("openResumeDesk", handleOpenResumeDesk);
