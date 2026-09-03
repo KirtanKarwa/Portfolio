1ec5bf9b61c162e67094ffe3291490c718c19a8b
KirtanKarwa <kirtankarwa2y@gmail.com>
2026-09-04 00:02:37 +0530

    Update preloader title to KirtanKarwa

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -57,7 +57,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          {config.developer.fullName}
+          KirtanKarwa
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
